c6b53122d87b4ecba8ec21a584cc85c3c6c559a2
Joel Tetrault <star99ers@gmail.com>
2014-03-20 19:55:02 -0500

    partial skeleton

---
+++ RadixTree/RedBlackTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadixTree
+{
+    class RedBlackTree : StringDictionary
+    {
+        public bool Search(String key)
+        {
+            return false;
+        }
+
+        public void Insert(String key)
+        {
+
+        }
+
+        public void Delete(String key)
+        {
+
+        }
+
+        public String Predecessor(String key)
+        {
+            return null;
+        }
+
+        public String Successor(String key)
+        {
+            return null;
+        }
+    }
+}
---
+++ RadixTree/StringDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadixTree
+{
+    interface StringDictionary
+    {
+        bool Search(String key);
+
+        void Insert(String key);
+
+        void Delete(String key);
+
+        String Predecessor(String key);
+
+        String Successor(String key);
+    }
+}
